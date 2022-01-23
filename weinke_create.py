cdc699017568504ffea50864b0f679640a9ca7cf
Evan Petzoldt <evan.petzoldt@protonmail.com>
2022-01-23 13:05:22 +0000

    general cleanup and weinke_create.py update

--- weinke_create.py
+++ weinke_create.py
@@ -1,103 +1,109 @@
-import os
-from sqlite3.dbapi2 import Timestamp
-from typing import Text
-from numpy import nan
-from pandas.core.reshape.merge import merge
 
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-from dotenv import load_dotenv
-import pickle
+from decouple import config, UndefinedValueError
 import pandas as pd
-import sqlite3
-import sys
+import mysql.connector
 from datetime import datetime, date, timedelta
-from slack_sdk import WebClient
 import dataframe_image as dfi
 
-query = """
-SELECT *
-FROM schedule_master
+# DB config
+db_config = {
+    "host":"f3stlouis.cac36jsyb5ss.us-east-2.rds.amazonaws.com",
+    "user":config('DATABASE_USER'), 
+    "password":config('DATABASE_WRITE_PASSWORD'),
+    "database":config('DATABASE_SCHEMA') 
+}
+
+# Figure out current and next weeks based on current start of day
+# I have the week start on Monday and end on Sunday - if this is run on Sunday, "current" week will start tomorrow
+tomorrow_day_of_week = (date.today() + timedelta(days=1)).weekday()
+current_week_start = date.today() + timedelta(days=-tomorrow_day_of_week+1)
+current_week_end = date.today() + timedelta(days=7-tomorrow_day_of_week)
+next_week_start = current_week_start + timedelta(weeks=1)
+next_week_end = current_week_end + timedelta(weeks=1)
+
+# Generate SQL pulls
+sql_current = f"""
+SELECT m.*, a.ao_display_name, a.ao_location_subtitle
+FROM schedule_master m
+LEFT JOIN schedule_aos a
+ON m.ao_channel_id = a.ao_channel_id
+WHERE m.event_date >= DATE('{current_week_start}')
+  AND m.event_date <= DATE('{current_week_end}');
 """
 
-with sqlite3.connect('data/schedule.db') as conn:
-    df = pd.read_sql_query(query, conn)
+sql_next = f"""
+SELECT m.*, a.ao_display_name, a.ao_location_subtitle
+FROM schedule_master m
+LEFT JOIN schedule_aos a
+ON m.ao_channel_id = a.ao_channel_id
+WHERE m.event_date >= DATE('{next_week_start}')
+  AND m.event_date <= DATE('{next_week_end}');
+"""
 
-df['date_time'] = pd.to_datetime(df['date'] + ' ' + df['time'], infer_datetime_format=True)
-df['date'] = df['date_time'].dt.strftime("%m/%d")  #.dt.date
-df['month_name'] = pd.DatetimeIndex(df['date_time']).month_name()
-df['day_of_week'] = pd.DatetimeIndex(df['date_time']).day_name()
-df['year_num'] = pd.DatetimeIndex(df['date_time']).year
-df['week_num'] = pd.Int64Index(pd.DatetimeIndex(df['date_time']).isocalendar().week)
-df['day_num'] = pd.DatetimeIndex(df['date_time']).day
+# Pull data
+try:
+  with mysql.connector.connect(**db_config) as mydb:
+    df_current = pd.read_sql_query(sql_current, mydb, parse_dates=['event_date'])
+    df_next = pd.read_sql_query(sql_next, mydb, parse_dates=['event_date'])
+except Exception as e:
+  print(f'There was a problem pull from the db: {e}')
 
-year_select = 2022
-week_select = 2
+df_list = [
+  [df_current, 'current_week_weinke'],
+  [df_next, 'next_week_weinke']
+]
 
-df = df[(df.year_num==year_select) & (df.week_num==week_select)]
-df.reset_index(inplace=True)
+for week in df_list:
+  df = week[0]
+  output_name = week[1]
 
-df.loc[0, 'pax_name'] = 'SlowPitch'
-df.loc[2, 'pax_name'] = 'Moneyball'
-df.loc[3, 'pax_name'] = 'DD'
-df.loc[4, 'pax_name'] = 'Tinkle'
-df.loc[5, 'pax_name'] = 'Default'
-df.loc[6, 'pax_name'] = 'Moneyball\nThe Forge'
-df.loc[6, 'time'] = '5:15'
-df.loc[7, 'pax_name'] = 'Elway'
-df.loc[8, 'pax_name'] = 'Moneyball'
-df.loc[9, 'pax_name'] = 'Audible'
-df.loc[10, 'pax_name'] = 'Moneyball'
-df.loc[11, 'pax_name'] = 'Clapper'
-df.loc[12, 'pax_name'] = 'Pothole'
-df.loc[13, 'pax_name'] = 'Peter Parker'
-df.loc[14, 'pax_name'] = 'Moneyball'
-df.loc[15, 'pax_name'] = 'SlowPitch'
+  # date operations
+  df['event_date_fmt'] = df['event_date'].dt.strftime("%m/%d")
 
-df.loc[df['pax_name'].isna(), 'pax_name'] = 'OPEN!'
-df['label'] = df['pax_name'] + '\n' + df['time']
+  # Reset index
+  df.reset_index(inplace=True)
 
-ao_list = pd.read_csv('data/ao_list.csv')
-df = pd.merge(df, ao_list, on=['ao'], how='left')
-df['AO\nLocation'] = df['ao_name_fmt'] + '\n' + df['ao_location']
+  # Build cell labels
+  df.loc[df['q_pax_name'].isna(), 'q_pax_name'] = 'OPEN!'
+  df['label'] = df['q_pax_name'] + '\n' + df['event_time']
+  df['AO\nLocation'] = df['ao_display_name'] + '\n' + df['ao_location_subtitle']
 
-df.sort_values(by=['AO\nLocation'], inplace=True)
-df2 = df.pivot(index='AO\nLocation', columns=['day_of_week', 'date'], values='label').fillna("")
+  # Reshape to wide format by date
+  df.sort_values(by=['AO\nLocation'], inplace=True)
+  df2 = df.pivot(index='AO\nLocation', columns=['event_day_of_week', 'event_date_fmt'], values='label').fillna("")
 
-df2.sort_index(axis=1, level=['date'], inplace=True)
-df2.columns = df2.columns.map('\n'.join).str.strip('\n')
-df2.reset_index(inplace=True)
+  # Sort and enforce word wrap on labels
+  df2.sort_index(axis=1, level=['event_date_fmt'], inplace=True)
+  df2.columns = df2.columns.map('\n'.join).str.strip('\n')
+  df2.reset_index(inplace=True)
 
-# Set CSS properties for th elements in dataframe
-th_props = [
-  ('font-size', '15px'),
-  ('text-align', 'center'),
-  ('font-weight', 'bold'),
-#   ('color', '#6d6d6d'),
-#   ('background-color', '#f7f7f9'),
-  ('color', '#F0FFFF'),
-  ('background-color', '#000000'),
-  ('white-space', 'pre-wrap'),
-  ('border', '1px solid #F0FFFF')
-  ]
+  # Set CSS properties for th elements in dataframe
+  th_props = [
+    ('font-size', '15px'),
+    ('text-align', 'center'),
+    ('font-weight', 'bold'),
+    ('color', '#F0FFFF'),
+    ('background-color', '#000000'),
+    ('white-space', 'pre-wrap'),
+    ('border', '1px solid #F0FFFF')
+    ]
 
-# Set CSS properties for td elements in dataframe
-td_props = [
-  ('font-size', '15px'),
-  ('text-align', 'center'),
-  ('white-space', 'pre-wrap'),
-  ('background-color', '#000000'),
-  ('color', '#F0FFFF'),
-  ('border', '1px solid #F0FFFF')
-  ]
+  # Set CSS properties for td elements in dataframe
+  td_props = [
+    ('font-size', '15px'),
+    ('text-align', 'center'),
+    ('white-space', 'pre-wrap'),
+    ('background-color', '#000000'),
+    ('color', '#F0FFFF'),
+    ('border', '1px solid #F0FFFF')
+    ]
 
-# Set table styles
-styles = [
-  dict(selector="th", props=th_props),
-  dict(selector="td", props=td_props)
-  ]
+  # Set table styles
+  styles = [
+    dict(selector="th", props=th_props),
+    dict(selector="td", props=td_props)
+    ]
 
-
-df_styled = df2.style.set_table_styles(styles).hide_index()
-
-dfi.export(df_styled,"mytable.png")
+  # set style and export png
+  df_styled = df2.style.set_table_styles(styles).hide_index()
+  dfi.export(df_styled,f"output/{output_name}.png")
